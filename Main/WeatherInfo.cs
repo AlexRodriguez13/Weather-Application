9013b7922ec562d7644afb8247dcf11a3781c3d8
AlexRodriguez13 <bryanalexandercb13@gmail.com>
2022-04-04 20:01:01 -0600

    root class

--- Main/WeatherInfo.cs
+++ Main/WeatherInfo.cs
@@ -6,28 +6,51 @@ using System.Threading.Tasks;
 
 namespace Main
 {
-   public  class WeatherInfo
+    public class WeatherInfo
     {
         public class coord
         {
             double lon { get; set; }
-            double lat { get; set; }    
+            double lat { get; set; }
 
         }
 
         public class weather
         {
-            string main { get;set; }    
+            string main { get; set; }
             string description { get; set; }
-            string icon { get; set; }   
+            string icon { get; set; }
 
         }
         public class main
         {
-            double temp { get; set; }   
-            double feels_like { get; set; }
-            double temp_min { get; set; }
+            double temp { get; set; }
+            //double feels_like { get; set; }
+            //double temp_min { get; set; }
+            //double temp_max { get; set; }
+            int preasure { get; set; }
+            int humidity { get; set; }
 
+        }
+        public class wind
+        {
+            double speed { get; set; }
+
+        }
+        public class sys
+        {
+            long sunrise { get; set; }
+            long sunset { get; set; }
+
+        }
+        public class root
+        {
+            public coord coord { get; set; }
+            public List<weather> weather  { get; set; }
+             public main main { get; set; }
+
+            public wind wind { get; set; }  
+            public sys sys { get; set; }   
         }
 
 
