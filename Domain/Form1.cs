66d54b285696bcec142c643606657c8705620413
AlexRodriguez13 <bryanalexandercb13@gmail.com>
2022-04-09 21:00:02 -0600

    Add Exception

--- Domain/Form1.cs
+++ Domain/Form1.cs
@@ -10,11 +10,14 @@ using System.Windows.Forms;
 using System.Net;
 using Newtonsoft.Json;
 using Main;
+using static Main.WeatherInfo;
 
 namespace Domain
 {
     public partial class Form1 : Form
     {
+        root Info;
+
         public Form1()
         {
             InitializeComponent();
@@ -37,23 +40,31 @@ namespace Domain
         }
          public void GetWeather()
         {
-            using (WebClient web = new WebClient())
+            try
             {
-                string url = string.Format("https://api.openweathermap.org/data/2.5/weather?q={0}&appid={1}", txtcity.Text,APIKey);
-                var Json = web.DownloadString(url);
-                WeatherInfo.root Info = JsonConvert.DeserializeObject<WeatherInfo.root>(Json);
+                using (WebClient web = new WebClient())
+                {
+                    string url = string.Format("https://api.openweathermap.org/data/2.5/weather?q={0}&appid={1}", txtcity.Text, APIKey);
+                    var Json = web.DownloadString(url);
+                    WeatherInfo.root Info = JsonConvert.DeserializeObject<WeatherInfo.root>(Json);
 
 
-                picIcon.ImageLocation = "https://openweathermap.org/img/w/" + Info.weather[0].icon + ".png";
-                lblcondicion.Text = Info.weather[0].main;
-                lbldetalles.Text = Info.weather[0].description;
-                lblsunset.Text = ConvertDateTime(Info.sys.sunset).ToShortTimeString();
-                lblsunrise.Text = ConvertDateTime(Info.sys.sunrise).ToShortTimeString();
+                    picIcon.ImageLocation = "https://openweathermap.org/img/w/" + Info.weather[0].icon + ".png";
+                    lblcondicion.Text = Info.weather[0].main;
+                    lbldetalles.Text = Info.weather[0].description;
+                    lblsunset.Text = ConvertDateTime(Info.sys.sunset).ToShortTimeString();
+                    lblsunrise.Text = ConvertDateTime(Info.sys.sunrise).ToShortTimeString();
 
-                lblwindspeed.Text = Info.wind.speed.ToString();
-                lblpressure.Text = Info.main.pressure.ToString();
+                    lblwindspeed.Text = Info.wind.speed.ToString();
+                    lblpressure.Text = Info.main.pressure.ToString();
 
+                }
             }
+            catch(Exception)
+            {
+                MessageBox.Show("Lo sentimos no hemos podido encontrar el lugar", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error); 
+            }
+            
         }
 
         DateTime ConvertDateTime(long milisec)
