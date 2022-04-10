699041802990efec3f498bae7378ecdea31340f5
AlexRodriguez13 <bryanalexandercb13@gmail.com>
2022-04-09 19:26:04 -0600

    new folder

---
+++ Main/WeatherInformation/main.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main.WeatherInformation
+{
+    public class main
+    {
+        public double temp { get; set; }
+
+        public int pressure { get; set; }
+        public int humidity { get; set; }
+    }
+}
